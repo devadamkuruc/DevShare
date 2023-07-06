d5c8c91c14c8077514dc7b08d7f4fdb5c85f682d
Adam Kuruc <me@adamkuruc.dev>
2023-07-06 22:58:28 +0200

    added pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { ProjectInterface } from "@/common.types";
 import Categories from "@/components/Categories";
+import LoadMore from "@/components/LoadMore";
 import ProjectCard from "@/components/ProjectCard";
 import { fetchAllProjects } from "@/lib/actions";
 
@@ -17,14 +18,22 @@ type ProjectSearch = {
 
 type SearchParams = {
   category?: string | null;
+  endcursor?: string;
 };
 
 type Props = {
   searchParams: SearchParams;
 };
 
-const Home = async ({ searchParams: { category } }: Props) => {
-  const data = (await fetchAllProjects(category as string)) as ProjectSearch;
+export const dynamic = "force-dynamic";
+export const dynamicParams = true;
+export const revalidate = 0;
+
+const Home = async ({ searchParams: { category, endcursor } }: Props) => {
+  const data = (await fetchAllProjects(
+    category as string,
+    endcursor
+  )) as ProjectSearch;
 
   const projectsToDisplay = data?.projectSearch?.edges || [];
 
@@ -38,6 +47,8 @@ const Home = async ({ searchParams: { category } }: Props) => {
       </section>
     );
   }
+
+  const pagination = data?.projectSearch?.pageInfo;
 
   return (
     <section className="flex-start flex-col paddings mb-16">
@@ -56,7 +67,13 @@ const Home = async ({ searchParams: { category } }: Props) => {
           />
         ))}
       </section>
-      <h1>LoadMore</h1>
+
+      <LoadMore
+        startCursor={pagination.startCursor}
+        endCursor={pagination.endCursor}
+        hasPreviousPage={pagination.hasPreviousPage}
+        hasNextPage={pagination.hasNextPage}
+      />
     </section>
   );
 };
--- lib/actions.ts
+++ lib/actions.ts
@@ -96,11 +96,11 @@ export const createNewProject = async (
 
 export const fetchAllProjects = async (
   category?: string,
-  endcursor?: string
+  endCursor?: string
 ) => {
   client.setHeader("x-api-key", apiKey);
 
-  return makeGraphQLRequest(projectsQuery, { category, endcursor });
+  return makeGraphQLRequest(projectsQuery, { category, endCursor });
 };
 
 export const getProjectDetails = async (id: string) => {
